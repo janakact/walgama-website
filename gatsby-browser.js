0460a1f4268d3d6f8dc95f70f9251b1021554222
Janaka <bjchathuranga@gmail.com>
2025-04-30 16:07:09 +0530

    Show fonts only after loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+//
+import WebFont from 'webfontloader';
+
+export const onClientEntry = () => {
+  // Add a loading class before font loads
+  document.documentElement.classList.add('fonts-loading');
+
+  WebFont.load({
+    custom: {
+      families: ["SinhalaAstro, DankFMBindumathiMono"],
+      urls: ['/fonts/fonts.css'],
+    },
+    active: () => {
+      document.documentElement.classList.remove('fonts-loading');
+      document.documentElement.classList.add('fonts-loaded');
+    },
+    inactive: () => {
+      document.documentElement.classList.remove('fonts-loading');
+    },
+  });
+};
